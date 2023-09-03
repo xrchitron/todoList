03f6a72a5da0867bc544946a87e47747faecae89
xrchitron <yuahochen1124@gmail.com>
2023-09-04 00:13:47 +0800

    build: use asyncHandler and async / await method to rebuild todos post

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const asyncHandler = require("express-async-handler");
 const app = express();
 const port = 3000;
 
@@ -30,12 +31,25 @@ app.get("/todos/new", (req, res) => {
   return res.render("new");
 });
 
-app.post("/todos", (req, res) => {
-  const name = req.body.name;
-  return Todo.create({ name })
-    .then(() => res.redirect("/todos"))
-    .catch((err) => console.log(err));
-});
+//class version
+// app.post("/todos", (req, res) => {
+//   const name = req.body.name;
+//   return Todo.create({ name })
+//     .then(() => res.redirect("/todos"))
+//     .catch((err) => console.log(err));
+// });
+
+//with asyncHandler and async / await method
+//console.log input value
+app.post(
+  "/todos",
+  asyncHandler(async (req, res) => {
+    const name = req.body.name;
+    const showName = await Todo.create({ name });
+    console.log(showName.toJSON());
+    res.redirect("/todos");
+  })
+);
 
 app.get("/todos/:id", (req, res) => {
   res.send(`get todo id: ${req.params.id}`);
