17e4c03773e2109714d7d8564d5a5be7392f05be
xrchitron <yuahochen1124@gmail.com>
2023-09-06 12:25:35 +0800

    feat: add error handling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const asyncHandler = require("express-async-handler");
+// const asyncHandler = require("express-async-handler");
 const methodOverride = require("method-override");
 const flash = require("connect-flash");
 const session = require("express-session");
@@ -29,123 +29,101 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-//class version
-// app.get("/todos", (req, res) => {
-//   return Todo.findAll({
-//     attributes: ["id", "name"],
-//     raw: true,
-//   })
-//     .then((todos) => res.render("todos", { todos }))
-//     .catch((err) => {
-//       res.status(422).json(err);
-//     });
-// });
-
-//with asyncHandler and async / await method
-app.get(
-  "/todos",
-  asyncHandler(async (req, res) => {
+app.get("/todos", async (req, res) => {
+  try {
     const todos = await Todo.findAll({
       attributes: ["id", "name", "isComplete"],
       raw: true,
     });
     res.render("todos", { todos, message: req.flash("success") });
-  })
-);
-
-app.get("/todos/new", (req, res) => {
-  return res.render("new");
+  } catch (error) {
+    console.log(error);
+    req.flash("error", "server error");
+    res.redirect("back");
+  }
 });
 
-//class version
-// app.post("/todos", (req, res) => {
-//   const name = req.body.name;
-//   return Todo.create({ name })
-//     .then(() => res.redirect("/todos"))
-//     .catch((err) => console.log(err));
-// });
+app.get("/todos/new", (req, res) => {
+  try {
+    return res.render("new", { error: req.flash("error") });
+  } catch (error) {
+    console.log(error);
+    req.flash("error", "server error");
+    res.redirect("back");
+  }
+});
 
-//with asyncHandler and async / await method
-//console.log input value
-app.post(
-  "/todos",
-  asyncHandler(async (req, res) => {
+app.post("/todos", async (req, res) => {
+  try {
     const name = req.body.name;
     const showName = await Todo.create({ name });
     console.log(showName.toJSON());
     req.flash("success", "Add successfully");
     res.redirect("/todos");
-  })
-);
+  } catch (error) {
+    console.log(error);
+    req.flash("error", "over-length");
+    // res.redirect("/todos/new"); is also fine
+    res.redirect("back");
+  }
+});
 
-// app.get("/todos/:id", (req, res) => {
-//   const id = req.params.id;
-//   return Todo.findByPk(id, {
-//     attributes: ["id", "name"],
-//     raw: true,
-//   })
-//     .then((todo) => res.render("todo", { todo }))
-//     .catch((err) => console.log(err));
-// });
-
-app.get(
-  "/todos/:id",
-  asyncHandler(async (req, res) => {
+app.get("/todos/:id", async (req, res) => {
+  try {
     const id = req.params.id;
     const todo = await Todo.findByPk(id, {
       attributes: ["id", "name", "isComplete"],
       raw: true,
     });
     res.render("todo", { todo, message: req.flash("success") });
-  })
-);
+  } catch (error) {
+    console.log(error);
+    req.flash("error", "server error");
+    res.redirect("back");
+  }
+});
 
-// app.get("/todos/:id/edit", (req, res) => {
-//   const id = req.params.id;
-//   return Todo.findByPk(id, {
-//     attributes: ["id", "name"],
-//     raw: true,
-//   }).then((todo) => res.render("edit", { todo }));
-// });
-
-app.get(
-  "/todos/:id/edit",
-  asyncHandler(async (req, res) => {
+app.get("/todos/:id/edit", async (req, res) => {
+  try {
     const id = req.params.id;
     const todo = await Todo.findByPk(id, {
       attributes: ["id", "name", "isComplete"],
       raw: true,
     });
     res.render("edit", { todo });
-  })
-);
+  } catch (error) {
+    console.log(error);
+    req.flash("error", "server error");
+    res.redirect("back");
+  }
+});
 
-// app.put("/todos/:id", (req, res) => {
-//   const name = req.body.name;
-//   const id = req.params.id;
-//   return Todo.update({ name }, { where: { id } }).then(() => res.redirect(`/todos/${id}`));
-// });
-
-app.put(
-  "/todos/:id",
-  asyncHandler(async (req, res) => {
+app.put("/todos/:id", async (req, res) => {
+  try {
     const { name, isComplete } = req.body;
     const id = req.params.id;
     await Todo.update({ name, isComplete: isComplete === "completed" }, { where: { id } });
     req.flash("success", "Update successfully");
     res.redirect(`/todos/${id}`);
-  })
-);
+  } catch (error) {
+    console.log(error);
+    req.flash("error", "server error");
+    res.redirect("back");
+  }
+});
 
-app.delete(
-  "/todos/:id",
-  asyncHandler(async (req, res) => {
+app.delete("/todos/:id", async (req, res) => {
+  try {
     const id = req.params.id;
     await Todo.destroy({ where: { id } });
     req.flash("success", "Delete successfully");
     res.redirect("/todos");
-  })
-);
+  } catch (error) {
+    console.log(error);
+    req.flash("error", "delete unsuccessfully");
+    res.redirect("back");
+  }
+});
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
