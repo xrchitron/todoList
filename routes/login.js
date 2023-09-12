138c8fbe2e76a2940d529e93de1be051a8dd2640
xrchitron <yuahochen1124@gmail.com>
2023-09-12 22:39:13 +0800

    rebuild: routes callback rebuild

--- routes/login.js
+++ routes/login.js
@@ -15,34 +15,60 @@ router.get("/register", (req, res) => {
   res.render("register");
 });
 
-router.post("/users", async (req, res, next) => {
-  try {
-    const { name, email, password, confirmPassword } = req.body;
-    if (!email || !password) {
-      req.flash("register", "email and password are required");
-      return res.redirect("back");
-    } else if (password !== confirmPassword) {
-      req.flash("register", "password doesn't match");
-      return res.redirect("back");
-    }
-    const checkEmail = await User.findAll({
-      where: { email },
-      raw: true,
-    });
-    if (checkEmail === []) {
-      req.flash("register", "email existed");
-      return res.redirect("back");
-    }
-    await User.create({ name, email, password });
-    req.flash("register", "register successfully");
-    res.redirect("login");
-  } catch (error) {
-    error.errorMessage = "register error";
-    next(error);
-  }
-});
+router.post("/users", register);
 
 router.post("/logout", (req, res) => {
   res.send("logout get in");
 });
+
+function checkRequiredFields(email, password, confirmPassword) {
+  if (!email || !password) {
+    return { status: false, message: "Email and password are required" };
+  }
+  if (password !== confirmPassword) {
+    return { status: false, message: "Passwords do not match" };
+  }
+  return;
+}
+async function checkExistingUser(email) {
+  const checkEmail = await User.findAll({
+    where: { email },
+    raw: true,
+  });
+  if (checkEmail.length === 0) {
+    return { status: false, message: "Email already exists" };
+  }
+  return;
+}
+async function createUser(name, email, password) {
+  await User.create({ name, email, password });
+  return { status: true, message: "User registered successfully" };
+}
+async function register(req, res, next) {
+  try {
+    const { name, email, password, confirmPassword } = req.body;
+
+    const checkFields = checkRequiredFields(email, password, confirmPassword);
+    if (!checkFields.status) {
+      req.flash("register", checkFields.message);
+      return res.redirect("back");
+    }
+
+    const checkUser = await checkExistingUser(email);
+    if (!checkUser.status) {
+      req.flash("register", checkUser.message);
+      return res.redirect("back");
+    }
+
+    const createUserStatus = await createUser(name, email, password);
+    if (createUserStatus.status) {
+      req.flash("register", createUserStatus.message);
+      return res.redirect("login");
+    }
+  } catch (error) {
+    error.errorMessage = "Registration error";
+    next(error);
+  }
+}
+
 module.exports = router;
